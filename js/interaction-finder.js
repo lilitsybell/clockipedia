a122731a8f0dfaab770a09094dc3bc516855c131
lilitsybell <christianwesthoff213@gmail.com>
2026-07-30 18:21:54 -0400

    Update interaction-finder.js

--- js/interaction-finder.js
+++ js/interaction-finder.js
@@ -25,6 +25,48 @@ const teamColors = {
 
 };
 
+const slugExceptions = {
+
+    "Organ Grinder": "organgrinder",
+    "Lil’ Monsta": "lilmonsta",
+    "Lil' Monsta": "lilmonsta",
+    "Pit-Hag": "pithag",
+    "Devil’s Advocate": "devilsadvocate",
+    "Devil's Advocate": "devilsadvocate",
+    "Hell’s Librarian": "hellslibrarian",
+    "Hell's Librarian": "hellslibrarian",
+    "Al-Hadikhia": "alhadikhia",
+    "Bounty Hunter": "bountyhunter",
+    "Cult Leader": "cultleader",
+    "Evil Twin": "eviltwin",
+    "Fortune Teller": "fortuneteller",
+    "High Priestess": "highpriestess",
+    "Lord of Typhon": "lordoftyphon",
+    "No Dashii": "nodashii",
+    "Poppy Grower": "poppygrower",
+    "Plague Doctor": "plaguedoctor",
+    "Scarlet Woman": "scarletwoman",
+    "Snake Charmer": "snakecharmer",
+    "Spirit of Ivory": "spiritofivory",
+    "Tea Lady": "tealady",
+    "Town Crier": "towncrier",
+    "Village Idiot": "villageidiot",
+    "Deus ex Fiasco": "deusexfiasco",
+    "Big Wig": "bigwig",
+    "God of Ug": "godofug",
+    "Storm Catcher": "stormcatcher",
+};
+
+function getSlug(name){
+
+    return slugExceptions[name] || name
+        .toLowerCase()
+        .replace(/[’']/g,"")
+        .replace(/[^a-z0-9]+/g,"-")
+        .replace(/^-|-$/g,"");
+
+}
+
 function fillDropdown(id){
 
     const select=document.getElementById(id);
