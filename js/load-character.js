2fd0e5a21c5d85b02dcbed644929ebefb0c8d272
lilitsybell <christianwesthoff213@gmail.com>
2026-07-30 12:58:48 -0400

    Update load-character.js

--- js/load-character.js
+++ js/load-character.js
@@ -1,4 +1,4 @@
-console.log("load-character updated 7/30/2026 12:54PM");
+console.log("load-character updated 7/30/2026 12:58PM");
 console.log("Character ID:", characterID);
 const teamColors = {
 
