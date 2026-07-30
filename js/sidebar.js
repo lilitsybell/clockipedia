be7ebf3088dcdad6cc4687a03dbe037afae0f187
lilitsybell <christianwesthoff213@gmail.com>
2026-07-29 23:07:38 -0400

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,107 +2,106 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const sidebar = document.getElementById("sidebar");
 
-    sidebar.innerHTML = `
+sidebar.innerHTML = `
 
-        <div class="logo">
+<div class="logo">
 
-            <img src="https://betterbotcwiki.weebly.com/uploads/1/4/0/2/140233688/better-botc-wiki-by-bell.png">
+<img src="https://betterbotcwiki.weebly.com/uploads/1/4/0/2/140233688/better-botc-wiki-by-bell.png">
 
-        </div>
-
-
-        <div class="search-box">
-
-            <input 
-                type="text"
-                placeholder="Search for a character..."
-            >
-
-        </div>
+</div>
 
 
-        <nav>
+<div class="search-box">
+
+<input 
+type="text"
+placeholder="Search for a character..."
+>
+
+</div>
 
 
-            <div class="nav-section">
+<nav>
 
-                <a href="/index.html" class="nav-link">
-                    Home
-                </a>
+<div class="nav-section">
 
-            </div>
+<a href="/index.html" class="nav-link">
+Home
+</a>
 
-
-            <div class="nav-section">
-
-                <div class="nav-title">
-                    Tools
-                </div>
-
-                <a href="/tools/interaction-finder.html" class="nav-link">
-                    Interaction Finder
-                </a>
-
-                <a href="/tools/script-generator.html" class="nav-link">
-                    Random Script Generator
-                </a>
-
-            </div>
+</div>
 
 
-            <div class="nav-section">
+<div class="nav-section">
 
-                <div class="nav-title">
-                    Characters
-                </div>
+<div class="nav-title">
+Tools
+</div>
 
-                <a href="/characters/townsfolk.html" class="nav-link">
-                    Townsfolk
-                </a>
+<a href="/tools/interaction-finder.html" class="nav-link">
+Interaction Finder
+</a>
 
-                <a href="/characters/outsiders.html" class="nav-link">
-                    Outsiders
-                </a>
+<a href="/tools/script-generator.html" class="nav-link">
+Random Script Generator
+</a>
 
-                <a href="/characters/minions.html" class="nav-link">
-                    Minions
-                </a>
-
-                <a href="/characters/demons.html" class="nav-link">
-                    Demons
-                </a>
-
-                <a href="/characters/travellers.html" class="nav-link">
-                    Travellers
-                </a>
-
-                <a href="/characters/loric.html" class="nav-link">
-                    Loric
-                </a>
-
-                <a href="/characters/fabled.html" class="nav-link">
-                    Fabled
-                </a>
-
-            </div>
+</div>
 
 
-            <div class="nav-section">
+<div class="nav-section">
 
-                <div class="nav-title">
-                    Other
-                </div>
+<div class="nav-title">
+Characters
+</div>
 
-                <a href="/contact.html" class="nav-link">
-                    Contact
-                </a>
+<a href="/characters/townsfolk.html" class="nav-link">
+Townsfolk
+</a>
 
-            </div>
+<a href="/characters/outsiders.html" class="nav-link">
+Outsiders
+</a>
+
+<a href="/characters/minions.html" class="nav-link">
+Minions
+</a>
+
+<a href="/characters/demons.html" class="nav-link">
+Demons
+</a>
+
+<a href="/characters/travellers.html" class="nav-link">
+Travellers
+</a>
+
+<a href="/characters/loric.html" class="nav-link">
+Loric
+</a>
+
+<a href="/characters/fabled.html" class="nav-link">
+Fabled
+</a>
+
+</div>
 
 
-        </nav>
+<div class="nav-section">
 
-    `;
+<div class="nav-title">
+Other
+</div>
+
+<a href="/contact.html" class="nav-link">
+Contact
+</a>
+
+</div>
+
+
+</nav>
+
+`;
 
 
     // Highlight current page
