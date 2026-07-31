395763edb2b0ddc227cd2f26a34e4efcb535abdf
lilitsybell <christianwesthoff213@gmail.com>
2026-07-31 14:12:37 -0400

    Update interaction-finder.js

--- js/interaction-finder.js
+++ js/interaction-finder.js
@@ -1,56 +1,5 @@
-console.log("interaction-finder Updated 7/31/26 1:50PM");
-let characters = {};
+console.log("interaction-finder Updated 7/31/26 14:12");
 let interactions = [];
-const teamColors = {
-    "Townsfolk":"blue",
-    "Outsider":"blue",
-    "Outsiders":"blue",
-    "Minion":"red",
-    "Minions":"red",
-    "Demon":"red",
-    "Demons":"red",
-    "Traveller":"traveller",
-    "Travellers":"traveller",
-    "Loric":"green",
-    "Fabled":"gold"
-};
-const slugExceptions = {
-    "Organ Grinder": "organgrinder",
-    "Lil’ Monsta": "lilmonsta",
-    "Lil' Monsta": "lilmonsta",
-    "Pit-Hag": "pithag",
-    "Devil’s Advocate": "devilsadvocate",
-    "Devil's Advocate": "devilsadvocate",
-    "Hell’s Librarian": "hellslibrarian",
-    "Hell's Librarian": "hellslibrarian",
-    "Al-Hadikhia": "alhadikhia",
-    "Bounty Hunter": "bountyhunter",
-    "Cult Leader": "cultleader",
-    "Evil Twin": "eviltwin",
-    "Fortune Teller": "fortuneteller",
-    "High Priestess": "highpriestess",
-    "Lord of Typhon": "lordoftyphon",
-    "No Dashii": "nodashii",
-    "Poppy Grower": "poppygrower",
-    "Plague Doctor": "plaguedoctor",
-    "Scarlet Woman": "scarletwoman",
-    "Snake Charmer": "snakecharmer",
-    "Spirit of Ivory": "spiritofivory",
-    "Tea Lady": "tealady",
-    "Town Crier": "towncrier",
-    "Village Idiot": "villageidiot",
-    "Deus ex Fiasco": "deusexfiasco",
-    "Big Wig": "bigwig",
-    "God of Ug": "godofug",
-    "Storm Catcher": "stormcatcher",
-};
-function getSlug(name){
-    return slugExceptions[name] || name
-        .toLowerCase()
-        .replace(/[’']/g,"")
-        .replace(/[^a-z0-9]+/g,"-")
-        .replace(/^-|-$/g,"");
-}
 function formatCharacters(text){
     return text.replace(/\[(.*?)\]/g, (_, character)=>{
         const slug = getSlug(character);
