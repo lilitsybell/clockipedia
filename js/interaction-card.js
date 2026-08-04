a3b3275e5d85ebfeb36d73f39bfbebed4a236229
lilitsybell <christianwesthoff213@gmail.com>
2026-08-03 23:05:46 -0400

    Update interaction-card.js

--- js/interaction-card.js
+++ js/interaction-card.js
@@ -1,1 +1,8 @@
 console.log("interactions Updated 8/03/26 22:55");
+function getCharacters(text){
+    const matches=text.match(/\[(.*?)\]/g);
+    if(!matches) return [];
+    return matches.map(match=>
+        match.slice(1,-1)
+    );
+}
