8aa0103c7ff2470eed50e307ba694567c006d5f1
lilitsybell <christianwesthoff213@gmail.com>
2026-08-04 10:18:51 -0400

    Update script-generator.js

--- js/script-generator.js
+++ js/script-generator.js
@@ -2,7 +2,7 @@ console.log("script-generator.js updated 8/04/26 09:55");
 async function initializeGenerator() {
     try {
         await loadOfficialCharacters();
-        console.log(data);
+        console.log(ScriptGenerator.officialCharacters);
         await loadScriptIndex();
         console.log("Generator ready.");
     }
