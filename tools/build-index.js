a1f9dc3e1162be257e3c8de1a0e7e96dfa564c7c
lilitsybell <christianwesthoff213@gmail.com>
2026-08-04 16:00:27 -0400

    Update build-index.js

--- tools/build-index.js
+++ tools/build-index.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 
+const scriptsPath = path.join(__dirname, "../data/scripts.json");
+const outputPath = path.join(__dirname, "../data/index.json");
+
+const scripts = JSON.parse(
+    fs.readFileSync(scriptsPath, "utf8")
+);
+
+const index = scripts.map(script => ({
+    id: script.id,
+    name: script.name,
+    author: script.author || "Unknown",
+    file: script.file,
+    tags: script.tags || [],
+    homebrew: script.homebrew || false,
+    size: script.size || "Full"
+}));
+
+fs.writeFileSync(
+    outputPath,
+    JSON.stringify(index, null, 4)
+);
+
+console.log(
+    `Generated index.json with ${index.length} scripts`
+);
