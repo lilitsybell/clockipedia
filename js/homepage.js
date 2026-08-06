b0cbb34ecb4f3d95153d07590ae6bfb0261940d3
lilitsybell <christianwesthoff213@gmail.com>
2026-08-06 00:23:26 -0400

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -150,7 +150,7 @@ function buildDailyCharacterWheel(){
         .filter(Boolean);
 
 
-    const visibleCount = 8;
+    const visibleCount = 10;
 
 
     const radius = 400;
@@ -159,8 +159,8 @@ function buildDailyCharacterWheel(){
     const centerY = 500;
 
 
-    const arcStart = Math.PI * 1.35;
-    const arcEnd = Math.PI * 2.00;
+    const arcStart = Math.PI * 1.15;
+    const arcEnd = Math.PI * 1.85;
 
 
     const tokens = [];
