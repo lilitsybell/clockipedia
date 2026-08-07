3bfe9d42b4f12b25129d1d7ad9f48631dc42f5fa
lilitsybell <christianwesthoff213@gmail.com>
2026-08-07 10:27:04 -0400

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -22,68 +22,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 </div>
         <nav class="sidebar-nav">
-
             <a href="/interaction-finder.html" class="nav-link">
                 Interactions
             </a>
-
-
             <a href="/script-generator.html" class="nav-link">
                 Scripts
             </a>
-            <div class="character-section">
-
-                <div class="character-toggle">
-                Characters
-                <span class="arrow">▼</span>
-                </div>
-
-
-                <div class="character-dropdown">
-
-                    <a href="/characters/townsfolk.html" class="nav-link sub-link">
-                        Townsfolk
-                    </a>
-
-                    <a href="/characters/outsiders.html" class="nav-link sub-link">
-                        Outsiders
-                    </a>
-
-                    <a href="/characters/minions.html" class="nav-link sub-link">
-                        Minions
-                    </a>
-
-                    <a href="/characters/demons.html" class="nav-link sub-link">
-                        Demons
-                    </a>
-
-                    <a href="/characters/travellers.html" class="nav-link sub-link">
-                        Travellers
-                    </a>
-
-                    <a href="/characters/loric.html" class="nav-link sub-link">
-                        Loric
-                    </a>
-
-                    <a href="/characters/fabled.html" class="nav-link sub-link">
-                        Fabled
-                    </a>
-
-                </div>
-
-            </div>
-
-
-
-            <a href="/contact.html" class="nav-link">
-                Contact
-            </a>
-
-
+   //         <a href="/characters.html" class="nav-link">
+   //             Characters
+   //         </a>
+   //         <a href="/contact.html" class="nav-link">
+   //             Contact
+   //         </a>
         </nav>
-
     `;
-
 
 // Highlight current page
 const currentPage = window.location.pathname;
